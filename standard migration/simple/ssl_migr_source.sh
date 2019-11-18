ab6eabf1c9660730aed9876b6be8f79302d69244
FFrabetti <frabettifil@gmail.com>
2019-11-18 13:35:19 -0500

    Update ssl_migr_source.sh

--- standard migration/simple/ssl_migr_source.sh
+++ standard migration/simple/ssl_migr_source.sh
@@ -1,35 +1,56 @@
 #!/bin/bash
 
 # to run a container from an image and test it:
-# 	docker run -d -p HOST_PORT:CONT_PORT REPO:TAG
+# 	docker run -d -p HOST_PORT:CONT_PORT IMAGE:TAG
 # 	curl localhost:HOST_PORT
 
-if [ $# -ne 3 ]; then
-	echo "Required 3 arguments: REPO TAG TO_HOST"
+# TODO: errors handling
+
+if [ $# -ne 2 ] && [ $# -ne 3 ]; then
+	echo "Required 2 or 3 arguments: TO_HOST (IMAGE TAG)|(CONTAINER)"
 	exit 1
 fi
 
-#CONTAINER='' # just for docker commit
-REPO=$1
-TAG=$2
+TO_HOST=$1
 USER='ubu2admin' # if the same it can be omitted
-TO_HOST=$3
+
+if [ $# -eq 2 ]; then
+	CONTAINER=$2
+	
+	# given a container, find out which image it is using
+	IMAGE_NAME=$(docker container inspect $CONTAINER -f '{{.Config.Image}}')
+	IMAGE=$(echo $IMAGE_NAME | cut -d: -f1)
+	TAG=$(echo $IMAGE_NAME | cut -d: -f2)
+else
+	IMAGE=$2
+	TAG=$3
+fi
 
 # replace / with .
-TAR_NAME=$(echo $REPO | sed 's/\//./g').$TAG.tar
+TAR_NAME=$(echo $IMAGE | sed 's/\//./g').$TAG.tar
 LOCAL_PATH=./$TAR_NAME
 REMOTE_DIR=/tmp/
 
-# new image from a container
-# docker commit $CONTAINER $REPO:$TAG
+# no need to commit for stateless service
+# {
+# new image from a container (default: container stopped while the commit is created)
+# docker commit $CONTAINER $IMAGE:$TAG
 # (no volumes)
+# }
+
+# TODO: skip docker save if a local backup for the same image already exists
+# - unique file name using the digest
+# - file name based on image name, but either calculate the digest or keep a mapping table
 
 # backup (tar) from an image
-docker save -o $LOCAL_PATH $REPO:$TAG
+docker save -o $LOCAL_PATH $IMAGE:$TAG
 # (no volumes)
 
 # send backup to destination
 scp $LOCAL_PATH $USER@$TO_HOST:$REMOTE_DIR
+
+# TODO: docker save and backup send operations may be pipelined (not with scp, though [?])
+# similar pipeline with reception and docker load
 
 # ################ execute script on peer ################
 # load image from tar
@@ -39,15 +60,17 @@ ssh $USER@$TO_HOST "docker load -i ${REMOTE_DIR}${TAR_NAME}"
 ssh $USER@$TO_HOST "docker image ls"
 
 # start container from image
-ssh $USER@$TO_HOST "docker run -d -p 8080:8080 $REPO:$TAG"
+ssh $USER@$TO_HOST "docker run -d -p 8080:8080 $IMAGE:$TAG"
+# TODO: generic port mapping
 
 # clean up
 ssh $USER@$TO_HOST "rm ${REMOTE_DIR}${TAR_NAME}"
+# or keep it for future uses (migrations FROM this node)
 
 # docker container ls -a
 # (docker ps -a)
 # docker container stop CONTAINER	# The main process inside the container will receive SIGTERM, and after a grace period, SIGKILL
 # docker container rm CONTAINER
-# docker image rm $REPO:$TAG
-# (docker rmi IMAGE)
+# docker image rm IMAGE_NAME
+# (docker rmi IMAGE_NAME)
 # docker image ls
