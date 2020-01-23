84079d102d4303de2c9157fb460075614794449f
FFrabetti <frabettifil@gmail.com>
2020-01-23 15:53:00 -0500

    Update setup.sh

--- experiments/utils/setup.sh
+++ experiments/utils/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cd /root/Cooperative-container-migration 
+rm -rf /root/bin
+cd /root/Cooperative-container-migration
+git reset --hard
 git pull
+(cd /root/Cooperative-container-migration/experiments/utils; mv setup.sh setup_git.sh) 
+chmod -R +x *
 cd ../
 mkdir -p /root/bin
 find . -type f \( -name '*.sh' -o -name '*.py' \) 2>/dev/null | while read name; do
