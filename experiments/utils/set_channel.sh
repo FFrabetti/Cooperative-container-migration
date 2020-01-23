96b2eec8e54e24597931ad13629e2aaf6817904f
FFrabetti <frabettifil@gmail.com>
2020-01-23 16:52:02 -0500

    Update set_channel.sh

--- experiments/utils/set_channel.sh
+++ experiments/utils/set_channel.sh
@@ -3,44 +3,39 @@
 
 source ./config.sh
 
-ip=()
-hname=()
 var=10ms;
 
-for v in master src dst client one two; do
-	ip+=($basenet${!v})
-	w1="node$v";
-	hname+=(${!w1})
-done
-#echo ${ip[@]}
-#echo ${hname[@]}
+function createParent {
+	sshroot $1 "tc qdisc del dev $ip_if root;
+				tc qdisc add dev $ip_if handle 1: root htb;
+				tc class add dev $ip_if parent 1: classid 1:1 htb rate 900Mbps ceil 900Mbps;"
+}
 
-function setValues {
-	local from=$1
+function doCycle {
+	local f=$1
 	shift
-	local to=0
-	sshroot ${hname[$from]} "sudo tc qdisc del dev $ip_if root; tc qdisc add dev $ip_if handle 1: root htb; tc class add dev $ip_if parent 1: classid 1:1 htb rate 900Mbps ceil 900Mbps;"
+
+	local index=0
 	while (( $# )); do
-		local delay=$(echo $1 | cut -d '-' -f1)
-		local bw=$(echo $1 | cut -d '-' -f2) 
-		setUniValues $from $to $delay $bw
-		to=$((to+1))
+		local t=$1
+		setValues $index $f ${!t} $2 $3
+		index=$((index+1))
+		shift
+		shift
 		shift
 	done
 }
 
-function setUniValues {
-	if [ $3 != "0" ]; then
-		echo "${hname[$1]} -> ${hname[$2]} delay=$3ms, bw=$4mbit"
-		local to=$2
-		sshroot ${hname[$1]} "tc class add dev $ip_if parent 1:1 classid 1:$((to+11)) htb rate $4mbit;
-			       tc filter add dev $ip_if parent 1: protocol ip prio 1 u32 match ip dst ${ip[$2]} flowid 1:$((to+11));
-			       tc qdisc add dev $ip_if parent 1:$((to+11)) handle $((10*(to+1))): netem delay $3ms;"
-	fi
+function setValues { 	# index from to delay bw
+	local i=$1
+	sshroot $2 "tc class add dev $ip_if parent 1:1 classid 1:$((i+11)) htb rate $5mbit;
+				tc filter add dev $ip_if parent 1: protocol ip prio 1 u32 match ip dst $3 flowid 1:$((i+11));
+				tc qdisc add dev $ip_if parent 1:$((i+11)) handle $((10*(i+1))): netem delay $4ms;"
 }
 
-from=0
-while read line; do
-	setValues $from $line < /dev/null
-	from=$((from+1))
+while read from line; do
+{
+	createParent ${!from}
+	doCycle ${!from} $line
+} < /dev/null
 done
